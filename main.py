2fe6b4ee235a1be5bf164be8a1a8d6c411474958
nicopan21 <npantojarojas@gmail.com>
2023-03-02 17:18:34 -0300

    add functionality of upgrade images

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from rembg import remove
 import os
+import cv2
+from cv2 import dnn_superres
+
 
 def remove_background(input_image_path, output_image_path):
     with open(input_image_path, 'rb') as input_image:
@@ -9,6 +12,29 @@ def remove_background(input_image_path, output_image_path):
             output_image.write(output)
     return None
 
+
+def upgrade_image(image):
+    # Create an SR object
+    sr = dnn_superres.DnnSuperResImpl_create()
+
+    # Read image
+    image = cv2.imread(image)
+
+    # Read the desired model
+    path = "Modulos/Models/EDSR_x4.pb"
+    sr.readModel(path)
+
+    # Set the desired model and scale to get correct pre- and post-processing
+    sr.setModel("edsr", 3)
+
+    # Upscale the image
+    result = sr.upsample(image)
+
+    #Save the image
+    cv2.imwrite("output/temp/temp.jpeg", result)
+    return result
+
+
 def main(input_folder = 'images', output_folder = 'output'):
     #? get input files and add relative path
     files = os.listdir(input_folder)
@@ -16,12 +42,17 @@ def main(input_folder = 'images', output_folder = 'output'):
     output_files = [f'{output_folder}/{file}' for file in files]
 
     for i, (input, output) in enumerate(zip(input_files, output_files)):
+        print(f'\nimage ({i+1}/{len(input_files)}): Upgrading image')
+        upgrade_image(input)
+        input = 'output/temp/temp.jpeg'
+
         print(f'image ({i+1}/{len(input_files)}): removing background')
         remove_background(
             input_image_path = input, 
             output_image_path = output
         )
     return None
+
 
 if __name__ == "__main__":
     #? run codes
